a1c5f66b04c2f9a8512afb1fd41d46567519e5dc
suapm095 <suapm095@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2008-10-02 22:50:48 +0000

    added tests for estimations with starting values provided by the user
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@743 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- trunk/frontier41/pkg/tests/frontierTest.R
+++ trunk/frontier41/pkg/tests/frontierTest.R
@@ -26,6 +26,12 @@ a4 <- frontier( front41Data, "firm", "time", "logOutput",
 print( a4 )
 print.default( a4 )
 
+a5 <- frontier( front41Data, "firm", "time", "logOutput",
+   c( "logCapital", "logLabour" ), mu = TRUE,
+   startVal = c( 0.5, 0.3, 0.5, 0.5, 0.9, -1 ) )
+print( a5 )
+print.default( a5 )
+
 
 data( riceProdPhil )
 riceProdPhil$lPROD  <- log( riceProdPhil$PROD )
@@ -73,3 +79,19 @@ b6 <- frontier( riceProdPhil,
    zNames = c( "EDYRS", "BANRAT" ), mu = TRUE )
 print( b6 )
 print.default( b6 )
+
+b7 <- frontier( riceProdPhil,
+   crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
+   yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
+   mu = TRUE, eta = TRUE,
+   startVal = c( -1, 0.3, 0.3, 0.3, 0.2, 0.5, -0.3, 0.1 ) )
+print( b7 )
+print.default( b7 )
+
+b8 <- frontier( riceProdPhil,
+   crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
+   yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
+   zNames = c( "EDYRS", "BANRAT" ), mu = TRUE, 
+   startVal = c( -1, 0.3, 0.3, 0.3, -3, -0.1, -4, 2, 0.8 ) )
+print( b8 )
+print.default( b8 )
