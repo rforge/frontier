afebf2cd2529480addc72071a79c6f75b13751d3
suapm095 <suapm095@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2008-10-02 21:50:42 +0000

    added a print method for objects of class frontier; added tests for this method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@741 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- trunk/frontier41/pkg/tests/frontierTest.R
+++ trunk/frontier41/pkg/tests/frontierTest.R
@@ -8,18 +8,22 @@ front41Data$logLabour  <- log( front41Data$labour )
 
 a1 <- frontier( front41Data, "firm", "time", "logOutput",
    c( "logCapital", "logLabour" ) )
+print( a1 )
 print.default( a1 )
 
 a2 <- frontier( front41Data, "firm", "time", "logOutput",
    c( "logCapital", "logLabour" ), mu = TRUE )
+print( a2 )
 print.default( a2 )
 
 a3 <- frontier( front41Data, "firm", "time", "logOutput",
    c( "logCapital", "logLabour" ), eta = TRUE )
+print( a3 )
 print.default( a3 )
 
 a4 <- frontier( front41Data, "firm", "time", "logOutput",
    c( "logCapital", "logLabour" ), mu = TRUE, eta = TRUE )
+print( a4 )
 print.default( a4 )
 
 
@@ -32,34 +36,40 @@ riceProdPhil$lNPK   <- log( riceProdPhil$NPK )
 b1 <- frontier( riceProdPhil,
    crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
    yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ) )
+print( b1 )
 print.default( b1 )
 
 b2 <- frontier( riceProdPhil,
    crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
    yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
    mu = TRUE )
+print( b2 )
 print.default( b2 )
 
 b3 <- frontier( riceProdPhil,
    crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
    yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
    eta = TRUE )
+print( b3 )
 print.default( b3 )
 
 b4 <- frontier( riceProdPhil,
    crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
    yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
    mu = TRUE, eta = TRUE )
+print( b4 )
 print.default( b4 )
 
 b5 <- frontier( riceProdPhil,
    crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
    yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
    zNames = c( "EDYRS", "BANRAT" ) )
+print( b5 )
 print.default( b5 )
 
 b6 <- frontier( riceProdPhil,
    crossSectionName = "FMERCODE", timePeriodName = "YEARDUM",
    yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
    zNames = c( "EDYRS", "BANRAT" ), mu = TRUE )
+print( b6 )
 print.default( b6 )
