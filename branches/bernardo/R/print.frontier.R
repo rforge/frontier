8f79ec78f4966b45d3191f1a9a4f45074f67d32f
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-03-16 11:34:25 +0000

    created branch for R code of Bernardo Mello
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@840 bfa1024f-619b-4c99-bedc-c79f27261e5f

---
+++ branches/bernardo/R/print.frontier.R
@@ -0,0 +1,24 @@
+print.frontier <- function( x, ... ) {
+
+   cat( "Stochastic Frontier Analysis with FRONTIER 4.1\n\n" )
+   if( x$modelType == 1 ) {
+      cat( "Error Components Frontier (see Battese & Coelli 1992)\n" )
+   } else {
+      cat( "Tech. Eff. Effects Frontier (see Battese & Coelli 1995)\n" )
+   }
+   if( x$ineffDecrease ) {
+      cat( "Inefficiency decreases the endogenous variable",
+	"(as in a production function)\n" )
+   } else {
+      cat( "Inefficiency increases the endogenous variable",
+	"(as in a cost function\n" )
+   }
+   if( x$logDepVar == 1 ) {
+      cat( "The dependent variable is logged\n" )
+   } else {
+      cat( "The dependent variable is not logged\n" )
+   }
+   cat( "\nFinal maximum likelihood estimates\n" )
+   print( coef( x ) )
+   invisible( x )
+}
