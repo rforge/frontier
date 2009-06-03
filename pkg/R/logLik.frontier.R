7911ed20052d976e59e334225c9ba51ffc9b7680
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-06-03 15:06:51 +0000

    the "logLik" method for objects of class "frontier" now returns the log likelihood value of the starting values for the parameters specified by the user using argument "startVal" if argument "which" is set to "start"; noted this in the documentation, improved/updated/corrected documentation of logLik.frontier
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1013 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/logLik.frontier.R
+++ pkg/R/logLik.frontier.R
@@ -4,10 +4,12 @@ logLik.frontier <- function( object, which = "mle", ... ) {
       result <- object$olsLogl
    } else if( tolower( which ) == "grid" ) {
       result <- object$gridLogl
+   } else if( tolower( which ) == "start" ) {
+      result <- object$startLogl
    } else if( tolower( which ) == "mle" ) {
       result <- object$mleLogl
    } else {
-      stop( "argument 'which' must be either 'ols', 'grid', or 'mle'" )
+      stop( "argument 'which' must be either 'ols', 'grid', 'start', or 'mle'" )
    }
    if( is.null( result ) ) {
       result <- NA
