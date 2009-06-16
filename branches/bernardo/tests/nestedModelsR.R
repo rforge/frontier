099bbb4e70526f1d8647deaa308e63515edfb3ef
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-06-16 18:37:56 +0000

    merged revision 1035 from /pkg/ to /branches/bernardo/ (added test script and corresponding output file with tests for models that can be estimated both by the ECF model and by the EEF model); added test script and corresponding output file with these tests using Bernado's R code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1036 bfa1024f-619b-4c99-bedc-c79f27261e5f

---
+++ branches/bernardo/tests/nestedModelsR.R
@@ -0,0 +1,47 @@
+library( frontier )
+
+## data set of rice producers in the Philippines
+data( riceProdPhil )
+riceProdPhil$lPROD  <- log( riceProdPhil$PROD )
+riceProdPhil$lAREA  <- log( riceProdPhil$AREA )
+riceProdPhil$lLABOR <- log( riceProdPhil$LABOR )
+riceProdPhil$lNPK   <- log( riceProdPhil$NPK )
+riceProdPhil$farm <-
+   paste( "F_", ifelse( riceProdPhil$FMERCODE > 9, "", "0" ),
+   riceProdPhil$FMERCODE, sep = "" )
+riceProdPhil$year <- riceProdPhil$YEARDUM + 1998
+riceProdPhil <- plm.data( riceProdPhil, c( "farm", "year" ) )
+
+
+########## cross-section data #############
+
+## without mu / zIntercept
+# Error Components Frontier (ECF)
+bb5ecf <- frontier( data = as.data.frame( riceProdPhil ),
+   yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ) )
+
+# Efficiency Effects Frontier (EEF)
+bb5eef <- frontier( data = as.data.frame( riceProdPhil ),
+   yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
+   modelType = "EEF" )
+
+# Comparisons
+all.equal( coef( bb5ecf ), coef( bb5eef ) )
+all.equal( vcov( bb5ecf ), vcov( bb5eef ) )
+all.equal( efficiencies( bb5ecf ), efficiencies( bb5eef ) )
+
+
+## with mu / zIntercept
+# Error Components Frontier (ECF)
+bb6ecf <- frontier( data = as.data.frame( riceProdPhil ),
+   yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
+   truncNorm = TRUE, code = "R" )
+
+# Efficiency Effects Frontier (EEF)
+bb6eef <- frontier( data = as.data.frame( riceProdPhil ),
+   yName = "lPROD", xNames = c( "lAREA", "lLABOR", "lNPK" ),
+   zIntercept = TRUE, modelType = "EEF", code = "R" )
+
+# Comparisons
+all.equal( coef( bb6ecf ), coef( bb6eef )[ c( 1:4, 6:7, 5 ) ] )
+all.equal( efficiencies( bb6ecf ), efficiencies( bb6eef ) )
