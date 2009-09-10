a92e3eb08d0c742fe4b43530005b3cce9035edaf
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-09-10 11:27:15 +0000

    when residuals.frontier() is called with argument "asInData" set to TRUE, the residuals are no longer taken from object$resid but are calculated in this function/method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1089 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/residuals.frontier.R
+++ pkg/R/residuals.frontier.R
@@ -2,15 +2,10 @@
 residuals.frontier <- function( object, asInData = FALSE, ... ) {
 
    if( asInData ) {
-      data <- eval( object$call$data )
-      if( "plm.dim" %in% class( data ) ) {
-         result <- rep( NA, nrow( data ) )
-         for( i in 1:nrow( data ) ) {
-            result[ i ] <- object$resid[ data[[ 1 ]][ i ], data[[ 2 ]][ i ] ]
-         }
-      } else {
-         result <- drop( object$resid )
-      }
+      result <- drop( object$dataTable[ , 3 ] -
+         cbind( rep( 1, nrow( object$dataTable ) ),
+            object$dataTable[ , 4:( object$nb + 3 ) ] ) %*%
+         coef( object )[ 1:( object$nb + 1 ) ] )
       names( result ) <- rownames( object$dataTable )
    } else {
       result <- object$resid
