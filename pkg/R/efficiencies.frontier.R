01383e1b573ea74c01d1ca30075e851808a3696e
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-09-10 13:00:57 +0000

    the elements of the vector of eficiency estimates returned by efficiencies( ..., asInData = TRUE ) have names equal to the observation names (stored in the returned element "dataTable") now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1090 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/efficiencies.frontier.R
+++ pkg/R/efficiencies.frontier.R
@@ -5,7 +5,6 @@ efficiencies.frontier <- function( object, asInData = FALSE, ... ) {
       data <- eval( object$call$data )
       if( "plm.dim" %in% class( data ) ) {
          result <- rep( NA, nrow( data ) )
-         names( result ) <- paste( data[[ 1 ]], data[[ 2 ]], sep = "_" )
          for( i in 1:nrow( data ) ) {
             if( ncol( object$effic ) == 1 ) {
                result[ i ] <- object$effic[ data[[ 1 ]][ i ], 1 ]
@@ -16,6 +15,7 @@ efficiencies.frontier <- function( object, asInData = FALSE, ... ) {
       } else {
          result <- drop( object$effic )
       }
+      names( result ) <- rownames( object$dataTable )
    } else {
       result <- object$effic
    }
