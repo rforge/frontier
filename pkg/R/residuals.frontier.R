b088f0bb11f32e031b5f56ed7b6233a125c560df
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-09-10 11:17:16 +0000

    the elements of the vector of residuals returned by residuals( ..., asInData = TRUE ) have names equal to the observation names (stored in the returned element "dataTable") now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1088 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/residuals.frontier.R
+++ pkg/R/residuals.frontier.R
@@ -5,13 +5,13 @@ residuals.frontier <- function( object, asInData = FALSE, ... ) {
       data <- eval( object$call$data )
       if( "plm.dim" %in% class( data ) ) {
          result <- rep( NA, nrow( data ) )
-         names( result ) <- paste( data[[ 1 ]], data[[ 2 ]], sep = "_" )
          for( i in 1:nrow( data ) ) {
             result[ i ] <- object$resid[ data[[ 1 ]][ i ], data[[ 2 ]][ i ] ]
          }
       } else {
          result <- drop( object$resid )
       }
+      names( result ) <- rownames( object$dataTable )
    } else {
       result <- object$resid
    }
