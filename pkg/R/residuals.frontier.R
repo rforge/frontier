791aa139a5e0a70cab02fb777061942d2c8a7819
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-09-11 09:02:22 +0000

    residuals.frontier() no longer uses object$resid but always calculates the residuals itself now (even if argument "asInData" is FALSE)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1097 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/residuals.frontier.R
+++ pkg/R/residuals.frontier.R
@@ -1,14 +1,24 @@
 # residuals of frontier models
 residuals.frontier <- function( object, asInData = FALSE, ... ) {
 
+   resid <- drop( object$dataTable[ , 3 ] -
+      cbind( rep( 1, nrow( object$dataTable ) ),
+         object$dataTable[ , 4:( object$nb + 3 ) ] ) %*%
+      coef( object )[ 1:( object$nb + 1 ) ] )
    if( asInData ) {
-      result <- drop( object$dataTable[ , 3 ] -
-         cbind( rep( 1, nrow( object$dataTable ) ),
-            object$dataTable[ , 4:( object$nb + 3 ) ] ) %*%
-         coef( object )[ 1:( object$nb + 1 ) ] )
+      result <- resid
       names( result ) <- rownames( object$dataTable )
    } else {
-      result <- object$resid
+      result <- matrix( NA, nrow = object$nn, ncol = object$nt )
+      if( length( resid ) != nrow( object$dataTable ) ) {
+         stop( "internal error: length of residuals is not equal to",
+            " the number of rows of the data table" )
+      }
+      for( i in 1:length( resid ) ) {
+         result[ object$dataTable[ i, 1 ], object$dataTable[ i, 2 ] ] <-
+            resid[ i ]
+      }
+      dimnames( result ) <- dimnames( object$resid )
    }
    return( result )
 }
