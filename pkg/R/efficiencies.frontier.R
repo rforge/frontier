ced85b097548e3faa4620a1e75c6c7bc334fab62
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-09-14 12:48:49 +0000

    efficiencies.frontier() calculates the efficiency estimates itself now even if the dependet variable is logged) (i.e. when this method is called with argument "asInData" set to TRUE and the model is an Error Components Frontier (ECF))
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1109 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/efficiencies.frontier.R
+++ pkg/R/efficiencies.frontier.R
@@ -2,7 +2,7 @@
 efficiencies.frontier <- function( object, asInData = FALSE, ... ) {
 
    if( asInData ) {
-      if( object$modelType == 1 && object$logDepVar ) {
+      if( object$modelType == 1 ) {
          resid <- residuals( object, asInData = TRUE )
          if( object$timeEffect ) {
             eta <- coef( object )[ "time" ]
@@ -43,9 +43,30 @@ efficiencies.frontier <- function( object, asInData = FALSE, ... ) {
          sigmaStarSq <- sigmaSq * gamma * ( 1 - gamma ) /
             ( 1 + ( tStar - 1 ) * gamma )
          sigmaStar <- sqrt( sigmaStarSq )
-         result <- ( pnorm( - dir * sigmaStar * etaStar + muStar / sigmaStar ) /
-            pnorm( muStar / sigmaStar ) ) *
-            exp( - dir * muStar * etaStar + 0.5 * sigmaStarSq * etaStar^2 )
+         if( object$logDepVar ) {
+            result <- ( pnorm( - dir * sigmaStar * etaStar + muStar / sigmaStar ) /
+               pnorm( muStar / sigmaStar ) ) *
+               exp( - dir * muStar * etaStar + 0.5 * sigmaStarSq * etaStar^2 )
+         } else {
+            fitted <- object$dataTable[ , 3 ] - resid
+            fittedStar <- rep( NA, object$nob )
+            tInd <- rep( NA, object$nob )
+            for( i in 1:object$nob ) {
+               fittedStar[ i ] <- sum( fitted[ object$dataTable[ , 1 ] ==
+                  object$dataTable[ i, 1 ] ] )
+               tInd[ i ] <- sum( object$dataTable[ , 1 ] ==
+                  object$dataTable[ i, 1 ] )
+            }
+            result <- 1 - dir * etaStar * ( muStar + sigmaStar *
+               exp( dnorm( muStar / sigmaStar, log = TRUE ) -
+                  pnorm( muStar / sigmaStar, log = TRUE ) ) ) /
+               ( fittedStar / tInd )
+         }
+         if( object$ineffDecrease ) {
+            result[ result > 1 ] <- 1
+         } else {
+            result[ result < 1 ] <- 1
+         }
       } else {
          data <- eval( object$call$data )
          if( "plm.dim" %in% class( data ) ) {
