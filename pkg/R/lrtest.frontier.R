f713ff66dab675f5364599c3815bfbbd0ef66c60
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2009-11-10 13:53:45 +0000

    if lrtest.frontier() is called with only one argument/object (i.e. argument "..." is not used), it compares the fitted model to a corresponding model without inefficiency (i.e. estimated by OLS)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1184 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/lrtest.frontier.R
+++ pkg/R/lrtest.frontier.R
@@ -10,62 +10,88 @@ lrtest.frontier <- function( object, ... ) {
    ## list of objects in ...
    objectList <- list( ... )
 
-   ## test if there are further objects in ...
-   if( length( objectList ) < 1 ){
-      stop( "at least one further argument ('...') must be provided" )
-   }
-
-   ## test if all objects are of class "frontier"
-   for( i in 1:length( objectList ) ) {
-      if( ! "frontier" %in% class( objectList[[ i ]] ) ){
-         stop( "all further arguments ('...') must be of class 'frontier'" )
-      }
-   }
-
-   ## get and save the names of the models (objects)
-   object$lrtest.frontier.name <- deparse( substitute( object ) )
-   dotsNames <- as.list( thisCall )[ -1 ]
-   dotsNames$object <- NULL
-   for( i in 1:length( objectList ) ){
-      objectList[[ i ]]$lrtest.frontier.name <- deparse( dotsNames[[ i ]] )
-   }
-   ## function to extract the names of the objects
-   extractName <- function( object ){
-      return( object$lrtest.frontier.name )
-   }
-
-   ## test if all models are of the same model type
-   for( i in 1:length( objectList ) ) {
-      if( object$modelType != objectList[[ i ]]$modelType ){
-         stop( "all models must be of the same type",
-            " but model '", extractName( object ), "' is an",
+   if( length( objectList ) < 1 ) {
+      ## if there is only one object, test for the inefficiency term
+      # obtain log likelihood values (including #DFs)
+      olsLogLik <- logLik( object, which = "OLS" )
+      mleLogLik <- logLik( object )
+      # prepare object to be returned
+      result <- matrix( NA, nrow = 2, ncol = 5 )
+      colnames( result ) <- c( "#Df", "LogLik", "Df", "Chisq", "Pr(>Chisq)" )
+      result <- as.data.frame( result )
+      # fill in #DF and log likelihood value of OLS estimation
+      result[ 1, "#Df" ] <- attributes( olsLogLik )$df
+      result[ 1, "LogLik" ] <- olsLogLik[ 1 ]
+      # fill in #DF and log likelihood value of MLE estimation
+      result[ 2, "#Df" ] <- attributes( mleLogLik )$df
+      result[ 2, "LogLik" ] <- mleLogLik[ 1 ]
+      # fill in #DF of the test, test statistic, and P-value
+      result[ 2, "Df" ] <- object$lrTestDf
+      result[ 2, "Chisq" ] <- object$lrTestVal
+      result[ 2, "Pr(>Chisq)" ] <- object$lrTestPval
+      # add appropriate class
+      class( result ) <- c( "anova", class( result ) )
+      # add heading for this test
+      attributes( result )$heading <- c( "Likelihood ratio test\n",
+         paste( "Model 1: OLS (no inefficiency)\nModel 2:",
             ifelse( object$modelType == 1,
-               " 'Error Components Frontier' (ECF)",
-               " 'Efficiency Effects Frontier' (EEF)" ),
-            ", while model '", extractName( objectList[[ i ]] ), "' is an",
-            ifelse( objectList[[ i ]]$modelType == 1,
-               " 'Error Components Frontier' (ECF)",
-               " 'Efficiency Effects Frontier' (EEF)" ) )
-      }
-   }
+               "Error Components Frontier (ECF)",
+               "Efficiency Effects Frontier (EEF)" ) ) )
+   } else {
+      ## if there is more than one object, compare the models
 
-   ## do the LR tests
-   result <- do.call( lrtest.default,
-      c( list( object = object ), objectList, list( name = extractName ) ) )
-
-   for( i in 2:nrow( result ) ){
-      if( ( result[ i, "#Df" ] - result[ i - 1, "#Df" ] ) *
-            ( result[ i, "LogLik" ] - result[ i - 1, "LogLik" ] ) < 0 ) {
-         if( result[ i, "LogLik" ] > result[ i - 1, "LogLik" ] ) {
-            compareLikelihood <- "larger"
-            compareDf <- "less"
-         } else {
-            compareLikelihood <- "smaller"
-            compareDf <- "more"
+      ## test if all objects are of class "frontier"
+      for( i in 1:length( objectList ) ) {
+         if( ! "frontier" %in% class( objectList[[ i ]] ) ){
+            stop( "all further arguments ('...') must be of class 'frontier'" )
          }
-         warning( "model '", i, "' has a ", compareLikelihood,
-            " log-likelihood value than the ", compareDf,
-            " restricted model '", i - 1, "'" )
+      }
+
+      ## get and save the names of the models (objects)
+      object$lrtest.frontier.name <- deparse( substitute( object ) )
+      dotsNames <- as.list( thisCall )[ -1 ]
+      dotsNames$object <- NULL
+      for( i in 1:length( objectList ) ){
+         objectList[[ i ]]$lrtest.frontier.name <- deparse( dotsNames[[ i ]] )
+      }
+      ## function to extract the names of the objects
+      extractName <- function( object ){
+         return( object$lrtest.frontier.name )
+      }
+
+      ## test if all models are of the same model type
+      for( i in 1:length( objectList ) ) {
+         if( object$modelType != objectList[[ i ]]$modelType ){
+            stop( "all models must be of the same type",
+               " but model '", extractName( object ), "' is an",
+               ifelse( object$modelType == 1,
+                  " 'Error Components Frontier' (ECF)",
+                  " 'Efficiency Effects Frontier' (EEF)" ),
+               ", while model '", extractName( objectList[[ i ]] ), "' is an",
+               ifelse( objectList[[ i ]]$modelType == 1,
+                  " 'Error Components Frontier' (ECF)",
+                  " 'Efficiency Effects Frontier' (EEF)" ) )
+         }
+      }
+
+      ## do the LR tests
+      result <- do.call( lrtest.default,
+         c( list( object = object ), objectList, list( name = extractName ) ) )
+
+      for( i in 2:nrow( result ) ){
+         if( ( result[ i, "#Df" ] - result[ i - 1, "#Df" ] ) *
+               ( result[ i, "LogLik" ] - result[ i - 1, "LogLik" ] ) < 0 ) {
+            if( result[ i, "LogLik" ] > result[ i - 1, "LogLik" ] ) {
+               compareLikelihood <- "larger"
+               compareDf <- "less"
+            } else {
+               compareLikelihood <- "smaller"
+               compareDf <- "more"
+            }
+            warning( "model '", i, "' has a ", compareLikelihood,
+               " log-likelihood value than the ", compareDf,
+               " restricted model '", i - 1, "'" )
+         }
       }
    }
 
