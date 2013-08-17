f2332638007088dbd1febe1546dc4982de08f72e
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2013-08-17 22:55:27 +0000

    added argument "extraPar" to coef.frontier()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1410 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/coef.frontier.R
+++ pkg/R/coef.frontier.R
@@ -1,15 +1,32 @@
-coef.frontier <- function( object, which = "mle", ... ) {
+coef.frontier <- function( object, which = "mle", extraPar = FALSE, ... ) {
+
+   if( length( extraPar ) != 1 || !is.logical( extraPar[1] ) ) {
+      stop( "argument 'extraPar' must be a single logical value" )
+   }
 
    if( tolower( which ) == "start" ){
-      return( object$startVal )
+      result <- object$startVal
    } else if( tolower( which ) == "ols" ) {
-      return( object$olsParam )
+      result <- object$olsParam
    } else if( tolower( which ) == "grid" ) {
-      return( object$gridParam )
+      result <- object$gridParam
    } else if( tolower( which ) == "mle" ) {
-      return( object$mleParam )
+      result <- object$mleParam
    } else {
       stop( "argument 'which' must be either 'start', 'ols', 'grid',",
          " or 'mle'" )
    }
+   
+   if( extraPar ) {
+      if( tolower( which ) == "ols" ) {
+         warning( "extra parameters are not available for coefficients",
+            " obtained by OLS" )
+      } else {
+         result <- c( result, 
+            sigmaSqU = unname( result[ "sigmaSq" ] * result[ "gamma"] ),
+            sigmaSqV = unname( result[ "sigmaSq" ] * ( 1 - result[ "gamma"] ) ) )
+      }
+   }
+   
+   return( result )
 }
