a8fefa834781265247b3e041366e9b442af98b4a
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2013-08-17 23:31:12 +0000

    added argument "extraPar" to vcov.frontier()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1412 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/vcov.frontier.R
+++ pkg/R/vcov.frontier.R
@@ -1,4 +1,22 @@
-vcov.frontier <- function( object, ... ) {
+vcov.frontier <- function( object, extraPar = FALSE, ... ) {
 
-   return( object$mleCov )
+   if( length( extraPar ) != 1 || !is.logical( extraPar[1] ) ) {
+      stop( "argument 'extraPar' must be a single logical value" )
+   }
+   
+   result <- object$mleCov
+   
+   if( extraPar ) {
+      jacobian <- diag( nrow( result ) )
+      jacobian <- rbind( jacobian, matrix( 0, nrow = 2, ncol = ncol( result ) ) )
+      rownames( jacobian ) <- c( rownames( result ), "sigmaSqU", "sigmaSqV" )
+      colnames( jacobian ) <- colnames( result )
+      jacobian[ "sigmaSqU", "sigmaSq" ] <- coef( object )[ "gamma" ]
+      jacobian[ "sigmaSqU", "gamma" ] <- coef( object )[ "sigmaSq" ]
+      jacobian[ "sigmaSqV", "sigmaSq" ] <- 1 - coef( object )[ "gamma" ]
+      jacobian[ "sigmaSqV", "gamma" ] <- - coef( object )[ "sigmaSq" ]
+      result <- jacobian %*% result %*% t( jacobian )
+   }
+
+   return( result )
 }
