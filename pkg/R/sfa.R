fe38d8b95eadcf63ed9fa9325723dded05355020
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2014-01-08 21:07:20 +0000

    moved code for preparing the "dataTable" into a separate function frontierDataTable()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1436 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/R/sfa.R
+++ pkg/R/sfa.R
@@ -142,8 +142,7 @@ sfa <- function(
       stop( "argument 'restartFactor' must be finite" )
    }
 
-
-   # preparing model matrix and model response
+   # obtain model frame for frontier model
    mc <- match.call( expand.dots = FALSE )
    m <- match( "data", names( mc ), 0 )
    mf <- mc[ c( 1, m ) ]
@@ -152,50 +151,8 @@ sfa <- function(
    mf$na.action <- na.pass
    mf[[ 1 ]] <- as.name( "model.frame" )
    mf <- eval( mf, parent.frame() )
-   mt <- attr( mf, "terms" )
-   xMat <- model.matrix( mt, mf )
-   xNames <- colnames( xMat )
-   yVec <- model.response( mf )
-   yName <- as.character( formula )[ 2 ]
-   if( length( yVec ) != nrow( xMat ) ) {
-      stop( "the number of observations of the endogenous variable (",
-         length( yVec ), ") is not equal to the number of observations",
-         " of the exogenous variables (", nrow( xMat ), ")" )
-   }
-
-   # cross section and time period identifier
-   if( "plm.dim" %in% class( data ) ) {
-      dataTable <- matrix( as.integer( data[[ 1 ]] ), ncol = 1 )
-      dataTable <- cbind( dataTable, as.integer( data[[ 2 ]] ) )
-   } else {
-      dataTable <- matrix( 1:length( yVec ), ncol = 1 )
-      dataTable <- cbind( dataTable, rep( 1, nrow( dataTable ) ) )
-   }
-   nb <- length( xNames )
-
-   # endogenous variable
-   dataTable <- cbind( dataTable, yVec )
-   if( sum( !is.na( yVec ) & is.finite( yVec ) ) == 0 ) {
-      stop( "the dependent variable has no valid observations" )
-   }
-
-   # exogenous variables
-   dataTable <- cbind( dataTable, xMat )
-   paramNames <- NULL
-   if( nb > 0 ) {
-      for( i in 1:nb ) {
-         paramNames <- c( paramNames, xNames[ i ] )
-         if( sum( !is.na( xMat[ , i ] ) & is.finite( xMat[ , i ] ) ) == 0 ) {
-            stop( "regressor '", xNames[ i ], "' has no valid observations" )
-         }
-      }
-   }
-
-   # variables explaining the efficiency level
-   if( is.null( effFormula  ) ) {
-      zNames <- NULL
-      zIntercept <- FALSE
-   } else {
+   # obtain model frame for frontier model
+   if( !is.null( effFormula  ) ) {
       if( class( effFormula ) != "formula" ) {
          stop( "argument 'effFormula' must be a formula" )
       } else if( length( effFormula ) != 2 ) {
@@ -208,92 +165,27 @@ sfa <- function(
       mfe$na.action <- na.pass
       mfe[[ 1 ]] <- as.name( "model.frame" )
       mfe <- eval( mfe, parent.frame() )
-      mte <- attr( mfe, "terms" )
-      zMat <- model.matrix( mte, mfe )
-      if( ncol( zMat ) > 0 && colnames( zMat )[ 1 ] == "(Intercept)" ) {
-         zIntercept <- TRUE
-         zMat <- zMat[ , -1, drop = FALSE ]
-      } else {
-         zIntercept <- FALSE
-      }
-      if( nrow( zMat ) != nrow( xMat ) ) {
-         stop( "the number of observations of the variables explaining",
-            " efficiency (", nrow( zMat ), ") is not equal to the number",
-            " of observations of the (regular) regressors (",
-            nrow( xMat ), ")" )
-      }
-      dataTable <- cbind( dataTable, zMat )
-      zNames <- colnames( zMat )
-      if( length( zNames ) > 0 ) {
-         for( i in 1:length( zNames ) ) {
-            if( sum( !is.na( zMat[ , i ] ) & is.finite( zMat[ , i ] ) ) == 0 ) {
-               stop( "the regressor for the inefficiency term '", zNames[ i ],
-                  "' has no valid observations" )
-            }
-         }
-      }
    }
-   nZvars <- length( zNames )
-
-   # detect and remove observations with NAs, NaNs, and INFs
-   validObs <- rowSums( is.na( dataTable ) | is.infinite( dataTable ) ) == 0
-   dataTable <- dataTable[ validObs, ]
-   # number of (valid) observations
-   nob <- sum( validObs )
-
-   # make sure that the cross-section units are numbered continously
-   firmId <- sort( unique( dataTable[ , 1 ] ) )
-   # number of cross-section units
-   nn <- length( firmId )
-   firmNo <- rep( NA, nrow( dataTable ) )
-   for( i in 1:nn ) {
-      firmNo[ dataTable[ , 1 ] == firmId[ i ] ] <- i
+   # prepare data table, etc
+   tmp <- frontierDataTable( formula = formula, effFormula = effFormula, 
+      data = data, mc = mc, mf = mf, mfe = mfe )
+   if( is.character( tmp ) ) {
+      stop( tmp )
    }
-   dataTable[ , 1 ] <- firmNo
-
-   # check consistency of firm numbers
-   if( any( is.na( dataTable[ , 1 ] ) ) ) {
-      stop( "internal error: at least one firm number is NA" )
-   }
-   if( min( dataTable[ , 1 ] ) != 1 ) {
-      stop( "internal error: the smallest firm number must be one" )
-   }
-   if( max( dataTable[ , 1 ] ) > nn ) {
-      stop( "internal error: a firm number is larger than the number of firms" )
-   }
-
-   # make sure that the time periods are numbered continously
-   timeId <- sort( unique( dataTable[ , 2 ] ) )
-   # number of time periods
-   nt <- length( unique( dataTable[ , 2 ] ) )
-   timeNo <- rep( NA, nrow( dataTable ) )
-   for( i in 1:nt ) {
-      timeNo[ dataTable[ , 2 ] == timeId[ i ] ] <- i
-   }
-   dataTable[ , 2 ] <- timeNo
-
-   # check consistency of time period numbers
-   if( any( is.na( dataTable[ , 2 ] ) ) ) {
-      stop( "internal error: at least one time period number is NA" )
-   }
-   if( min( dataTable[ , 2 ] ) != 1 ) {
-      stop( "internal error: the smallest time period number must be one" )
-   }
-   if( max( dataTable[ , 2 ] ) > nt ) {
-      stop( "internal error: a time period number is larger",
-         " than the number of time periods" )
-   }
-
-   # check for double entries for firm/period combinations
-   for( i in 1:nn ) {
-      for( j in 1:nt ) {
-         if( sum( dataTable[ , 1 ] == i & dataTable[ , 2 ] == j ) > 1 ){
-            stop( "more than one observation for firm '", firmId[ i ],
-               "' in period '", timeId[ j ], "'" )
-         }
-      }
-   }
-
+   dataTable  <- tmp$dataTable
+   validObs   <- tmp$validObs
+   xMat       <- tmp$xMat
+   firmId     <- tmp$firmId
+   timeId     <- tmp$timeId
+   obsNames   <- tmp$obsNames
+   nb         <- tmp$nb
+   nob        <- tmp$nob
+   nn         <- tmp$nn
+   nt         <- tmp$nt
+   paramNames <- tmp$paramNames
+   zNames     <- tmp$zNames
+   zIntercept <- tmp$zIntercept
+   nZvars     <- tmp$nZvars
 
    # mu: truncNorm, zIntercept
    if( modelType == 1 ) {
@@ -308,24 +200,6 @@ sfa <- function(
    } else {
       eta <- nZvars
    }
-
-   # adding column names to the data table
-   colnames( dataTable ) <- c( "id", "t", yName, xNames, zNames )
-
-   # obtaining names of the observations
-   if( !is.null( rownames( data ) ) ) {
-      obsNames <- rownames( data )
-   } else if( !is.null( names( yVec ) ) ) {
-      obsNames <- names( yVec )
-   } else if( !is.null( rownames( xMat ) ) ) {
-      obsNames <- rownames( xMat )
-   } else if( !is.null( rownames( zMat ) ) ) {
-      obsNames <- rownames( zMat )
-   } else {
-      obsNames <- NULL
-   }
-   rownames( dataTable ) <- obsNames[ validObs ]
-   names( validObs ) <- obsNames
 
    nParamTotal <- nb + 2 + mu + eta
    if( nParamTotal > nob ) {
