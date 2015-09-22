feb13cce06365eb133aebe4dfbf78e02a87ebcfa
arne <arne@bfa1024f-619b-4c99-bedc-c79f27261e5f>
2015-09-22 20:45:37 +0000

    added further tests: with local-linear regression and/or with other bandwidth selection methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/frontier@1477 bfa1024f-619b-4c99-bedc-c79f27261e5f

--- pkg/npFrontier/tests/FLW_Sims.R
+++ pkg/npFrontier/tests/FLW_Sims.R
@@ -27,10 +27,56 @@ x <- rchisq(n,1)
 
 y <- 1+log(1+x)+e
 
-sfa.flw <- FLW(y=y,x=x,regtype="lc",bw.sel="rot")  # ,dis="ntn"
+# local-constant regression, rule-of-thumb bandwidths
+sfa.flw <- FLW(y=y,x=x,bw.sel="rot")  # ,dis="ntn"
 
 for( i in 1:length( sfa.flw ) ) {
   cat( "$", names( sfa.flw )[ i ], "\n", sep = "" )
   print( round( sfa.flw[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-linear regression, rule-of-thumb bandwidths
+sfa.flw.ll <- FLW( y = y, x = x, regtype = "ll", bw.sel = "rot" )
+
+for( i in 1:length( sfa.flw.ll ) ) {
+  cat( "$", names( sfa.flw.ll )[ i ], "\n", sep = "" )
+  print( round( sfa.flw.ll[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-constant regression, ls.cv bandwidths
+sfa.flw.ls <- FLW( y = y, x = x )
+
+for( i in 1:length( sfa.flw.ls ) ) {
+  cat( "$", names( sfa.flw.ls )[ i ], "\n", sep = "" )
+  print( round( sfa.flw.ls[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-linear regression, ls.cv bandwidths
+sfa.flw.ll.ls <- FLW( y = y, x = x, regtype = "ll" )
+
+for( i in 1:length( sfa.flw.ll.ls ) ) {
+  cat( "$", names( sfa.flw.ll.ls )[ i ], "\n", sep = "" )
+  print( round( sfa.flw.ll.ls[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-constant regression, aic bandwidths
+sfa.flw.aic <- FLW( y = y, x = x, bw.sel = "cv.aic" )
+
+for( i in 1:length( sfa.flw.aic ) ) {
+  cat( "$", names( sfa.flw.aic )[ i ], "\n", sep = "" )
+  print( round( sfa.flw.aic[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-linear regression, aic bandwidths
+sfa.flw.ll.aic <- FLW( y = y, x = x, regtype = "ll", bw.sel = "cv.aic" )
+
+for( i in 1:length( sfa.flw.ll.aic ) ) {
+  cat( "$", names( sfa.flw.ll.aic )[ i ], "\n", sep = "" )
+  print( round( sfa.flw.ll.aic[[ i ]], 2 ) )
   cat( "\n" )
 }
--- pkg/npFrontier/tests/FLW_test.R
+++ pkg/npFrontier/tests/FLW_test.R
@@ -5,12 +5,68 @@ library( "npFrontier" )
 # load data
 data( front41Data, package = "frontier" )
 
-# semiparametric local-constant Cobb-Douglas production frontier
+# semiparametric Cobb-Douglas production frontiers
+# local-constant regression, ls.cv bandwidths
 FLW_Result <- FLW( log( front41Data$output ), 
   cbind( log( front41Data$capital ), log( front41Data$labour ) ) )
 
 for( i in 1:length( FLW_Result ) ) {
   cat( "$", names( FLW_Result )[ i ], "\n", sep = "" )
   print( round( FLW_Result[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-linear regression, ls.cv bandwidths
+FLW_Result_ll <- FLW( log( front41Data$output ), 
+  cbind( log( front41Data$capital ), log( front41Data$labour ) ),
+  regtype = "ll" )
+
+for( i in 1:length( FLW_Result_ll ) ) {
+  cat( "$", names( FLW_Result_ll )[ i ], "\n", sep = "" )
+  print( round( FLW_Result_ll[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-constant regression, rule-of-thumb bandwidths
+FLW_Result_rot <- FLW( log( front41Data$output ), 
+  cbind( log( front41Data$capital ), log( front41Data$labour ) ),
+  bw.sel = "rot"  )
+
+for( i in 1:length( FLW_Result_rot ) ) {
+  cat( "$", names( FLW_Result_rot )[ i ], "\n", sep = "" )
+  print( round( FLW_Result_rot[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-linear regression with rule-of-thumb bandwidths
+FLW_Result_ll_rot <- FLW( log( front41Data$output ), 
+  cbind( log( front41Data$capital ), log( front41Data$labour ) ),
+  regtype = "ll", bw.sel = "rot"  )
+
+for( i in 1:length( FLW_Result_ll_rot ) ) {
+  cat( "$", names( FLW_Result_ll_rot )[ i ], "\n", sep = "" )
+  print( round( FLW_Result_ll_rot[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-constant regression, ls.cv bandwidths
+FLW_Result_aic <- FLW( log( front41Data$output ), 
+  cbind( log( front41Data$capital ), log( front41Data$labour ) ),
+  bw.sel = "cv.aic" )
+
+for( i in 1:length( FLW_Result_aic ) ) {
+  cat( "$", names( FLW_Result_aic )[ i ], "\n", sep = "" )
+  print( round( FLW_Result_aic[[ i ]], 2 ) )
+  cat( "\n" )
+}
+
+# local-linear regression, ls.cv bandwidths
+FLW_Result_ll_aic <- FLW( log( front41Data$output ), 
+  cbind( log( front41Data$capital ), log( front41Data$labour ) ),
+  regtype = "ll", bw.sel = "cv.aic" )
+
+for( i in 1:length( FLW_Result_ll_aic ) ) {
+  cat( "$", names( FLW_Result_ll_aic )[ i ], "\n", sep = "" )
+  print( round( FLW_Result_ll_aic[[ i ]], 2 ) )
   cat( "\n" )
 }
